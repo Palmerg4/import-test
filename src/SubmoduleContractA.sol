4a22e6bddf0098e8937a5cd65744425bcd301214
Palmerg4 <71123289+Palmerg4@users.noreply.github.com>
2024-05-21 09:48:07 -0400

    Added submodule contract; Set explicit EVM version

---
+++ src/SubmoduleContractA.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+//import "lib/import-test/src/ContractB.sol";
+//import "lib/import-test/src/ContractC.sol";
+
+contract SubmoduleContractA {}
